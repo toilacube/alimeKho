52ae68b335742f76606710e1aefbedc11cae29b2
Groolt <binhnguyen9803@gmail.com>
2023-05-28 23:03:20 +0700

    /newupd/

--- app/src/main/java/com/example/alimekho/Adapter/SupplierAdapter.java
+++ app/src/main/java/com/example/alimekho/Adapter/SupplierAdapter.java
@@ -5,60 +5,58 @@ import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.RecyclerView.Adapter;
 
-import com.example.alimekho.Activity.CreatePNK2Activity;
 import com.example.alimekho.DataBase.SQLServerConnection;
 import com.example.alimekho.Model.nhaCungCap;
-import com.example.alimekho.Model.sanPham;
+import com.example.alimekho.Model.phieuNhapKho;
 import com.example.alimekho.R;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
-public class PNK2Adapter extends Adapter<PNK2Adapter.PNK2ViewHolder>{
+public class SupplierAdapter extends RecyclerView.Adapter<SupplierAdapter.SupplierViewHolder> {
     private Context context;
     private ArrayList<nhaCungCap> nhaCungCaps;
-    private setData setData;
-    private nhaCungCap nhaCungCapVV;
-    public PNK2Adapter(Context context, ArrayList<nhaCungCap> nhaCungCaps) {
+    private ArrayList<nhaCungCap> listChecked;
+    private boolean isSelectedAll = false;
+
+    public SupplierAdapter(Context context, ArrayList<nhaCungCap> nhaCungCaps) {
         this.context = context;
         this.nhaCungCaps = nhaCungCaps;
-        this.setData = (PNK2Adapter.setData) context;
-    }
-
-    public interface setData {
-        void setNhaCungCap(nhaCungCap nhaCungCap);
+        listChecked = new ArrayList<>();
     }
 
     @NonNull
     @Override
-    public PNK2ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_pnk2, parent, false);
-        return new PNK2ViewHolder(view);
+    public SupplierViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_supplier, parent, false);
+        return new SupplierViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PNK2ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull SupplierViewHolder holder, int position) {
         nhaCungCap nhaCungCap = nhaCungCaps.get(position);
         holder.txtmaNCC.setText(nhaCungCap.getMaNCC().toString().trim());
         holder.txttenNCC.setText(nhaCungCap.getTenCC().toString().trim());
         holder.txtdiaChi.setText(nhaCungCap.getDiaChi().toString().trim());
         holder.txtSDT.setText(nhaCungCap.getSDT().toString().trim());
-        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+        holder.cb.setChecked(isSelectedAll);
+        holder.cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                nhaCungCapVV = (new nhaCungCap(nhaCungCap.getMaNCC().toString().trim(), nhaCungCap.getTenCC().toString().trim(),
-                        nhaCungCap.getDiaChi().toString().trim(), nhaCungCap.getSDT().toString()));
-                setData.setNhaCungCap(nhaCungCapVV);
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (compoundButton.isChecked()) listChecked.add(nhaCungCap);
+                else listChecked.remove(nhaCungCap);
             }
         });
         holder.linearLayout.setOnLongClickListener(new View.OnLongClickListener() {
@@ -71,7 +69,6 @@ public class PNK2Adapter extends Adapter<PNK2Adapter.PNK2ViewHolder>{
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         nhaCungCaps.remove(nhaCungCap);
-                        setData.setNhaCungCap(new nhaCungCap("", "", "", ""));
                         notifyDataSetChanged();
                         SQLServerConnection db = new SQLServerConnection();
                         Connection conn = db.getConnection();
@@ -101,25 +98,52 @@ public class PNK2Adapter extends Adapter<PNK2Adapter.PNK2ViewHolder>{
             }
         });
     }
+
     @Override
     public int getItemCount() {
         return nhaCungCaps == null ? 0 : nhaCungCaps.size();
     }
 
-    public class PNK2ViewHolder extends RecyclerView.ViewHolder{
+    public class SupplierViewHolder extends RecyclerView.ViewHolder{
         private TextView txtmaNCC, txttenNCC, txtdiaChi, txtSDT;
         private LinearLayout linearLayout;
-        public PNK2ViewHolder(@NonNull View itemView) {
+        private CheckBox cb;
+        public SupplierViewHolder(@NonNull View itemView) {
             super(itemView);
-            txtmaNCC = itemView.findViewById(R.id.custompnk2_txtmaNCC);
-            txttenNCC = itemView.findViewById(R.id.custompnk2_txttenNCC);
-            txtdiaChi = itemView.findViewById(R.id.custompnk2_txtdiaChi);
-            txtSDT = itemView.findViewById(R.id.custompnk2_txtSDT);
-            linearLayout = itemView.findViewById(R.id.custompnk_ln);
+            txtmaNCC = itemView.findViewById(R.id.customspl_txtmaNCC);
+            txttenNCC = itemView.findViewById(R.id.customspl_txttenNCC);
+            txtdiaChi = itemView.findViewById(R.id.customspl_txtdiaChi);
+            txtSDT = itemView.findViewById(R.id.customspl_txtSDT);
+            cb = itemView.findViewById(R.id.check_box);
+            linearLayout = itemView.findViewById(R.id.customspl_ln);
         }
     }
     public void setFilteredList(ArrayList<nhaCungCap> filteredList) {
         this.nhaCungCaps = filteredList;
+        notifyDataSetChanged();
+    }
+    public void setCheckAll(boolean is_checked){
+        isSelectedAll = is_checked;
+        notifyDataSetChanged();
+    }
+    public void deleteCheckedItems() {
+        SQLServerConnection db = new SQLServerConnection();
+        Connection conn = db.getConnection();
+        for (nhaCungCap nhaCungCap : listChecked) {
+            nhaCungCaps.remove(nhaCungCap);
+            try {
+                String delete = "UPDATE supplier\n" +
+                        "SET is_deleted = 1\n" +
+                        " WHERE id = ?";
+                PreparedStatement stm = conn.prepareStatement(delete);
+                stm.setInt(1, Integer.parseInt(nhaCungCap.getMaNCC()));
+                int rs = stm.executeUpdate();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        listChecked.clear();
         notifyDataSetChanged();
     }
 }
