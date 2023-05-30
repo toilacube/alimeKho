d8ed2abff1568fee5a352cecf1a5d8765b99d155
Nguyễn Sỹ Lê Hoàng <21520870@gm.uit.edu.vn>
2023-05-31 00:28:56 +0700

    adjsut merge

--- app/src/main/java/com/example/alimekho/Activity/HomeActivity.java
+++ app/src/main/java/com/example/alimekho/Activity/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.alimekho.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,13 +12,17 @@ import com.example.alimekho.Model.Employee;
 import com.example.alimekho.R;
 
 public class HomeActivity extends AppCompatActivity {
-    CardView cvSanPham, cvNhapKho, cvXuatKho, cvNhanVien
+    CardView cvSanPham, cvNhapKho, cvXuatKho, cvNhanVien,cvBatch, cvOutLocation, cvKiemKho, cvNhaCC
             , cvViTri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        cvNhaCC = findViewById(R.id.cvSupplier);
+        cvKiemKho = findViewById(R.id.cvKiemKho);
+        cvOutLocation = findViewById(R.id.cvDiaDiemXuat);
+        cvBatch = findViewById(R.id.cvBatch);
         cvSanPham = findViewById(R.id.cv_san_pham);
         cvNhanVien = findViewById(R.id.cv_nhan_vien);
         cvNhapKho = findViewById(R.id.cv_nhap_kho);
@@ -28,6 +33,34 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void toActivity() {
+        cvNhaCC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SupplierActivity.class);
+                startActivity(intent);
+            }
+        });
+        cvKiemKho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), QLPKKActivity.class);
+                startActivity(intent);
+            }
+        });
+        cvOutLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SupermarketActivity.class);
+                startActivity(intent);
+            }
+        });
+        cvBatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BatchActivity.class);
+                startActivity(intent);
+            }
+        });
         cvViTri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
